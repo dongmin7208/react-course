dd560e35574298a356f2c5ddf533bdcb071e3d90
dongmin7208 <sayg3o3d@gmail.com>
2023-01-07 21:46:47 +0900

    add: firebase data

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupPage from './pages/NewMeetup';
-import MainNavigation from './components/layout/MainNavigation';
-
+import Layout from './components/layout/Layout';
 function App() {
   // localhost:3000
   // my-page.com/
   return (
     <div>
-      <MainNavigation />
-      <Switch>
-        <Route path='/' exact>
-          <AllMeetupsPage />
-        </Route>
-        <Route path='/new-meetup'>
-          <NewMeetupPage />
-        </Route>
-        <Route path='/favorites'>
-          <FavoritesPage />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path='/' exact>
+            <AllMeetupsPage />
+          </Route>
+          <Route path='/new-meetup'>
+            <NewMeetupPage />
+          </Route>
+          <Route path='/favorites'>
+            <FavoritesPage />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,4 +1,26 @@
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+import { useHistory } from 'react-router-dom';
 function NewMeetupPage() {
-  return <div>NewMeetup Page</div>;
+  const history = useHistory();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-getting-started-22e33-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    ).then(() => {
+      history.replace('/');
+    });
+  }
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 export default NewMeetupPage;
