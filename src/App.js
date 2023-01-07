2a1bdd162ccc42da1b9423cf4fe33c9ec491d08d
dongmin7208 <sayg3o3d@gmail.com>
2023-01-07 15:55:31 +0900

    add: Css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupPage from './pages/NewMeetup';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   // localhost:3000
   // my-page.com/
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path='/' exact>
           <AllMeetupsPage />
