0ccb03acd73dd8cb1ce2e2d259175c164b6b9826
dongmin7208 <sayg3o3d@gmail.com>
2023-01-07 14:48:04 +0900

    add: router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import Todo from './components/Todo';
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import FavoritesPage from './pages/Favorites';
+import NewMeetupPage from './pages/NewMeetup';
+
 function App() {
+  // localhost:3000
+  // my-page.com/
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text='text1' />
-      <Todo text='text2' />
-      <Todo text='text3' />
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
